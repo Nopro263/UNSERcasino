fa7a8de14f2949dba6841aa9eda2c17bb2928f4d
Noah <82201000+Nopro263@users.noreply.github.com>
2024-04-17 10:57:30 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using NoahCardOutput;
+
+namespace UNSERcasino
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CardPrinter.print(PrintableCard.FromKarte("König Herz"));
+            Console.ReadLine();
+        }
+    }
+}
