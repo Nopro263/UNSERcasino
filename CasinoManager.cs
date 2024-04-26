d5f70d4368b729010e61ae6fe1de6e31bc5158ac
Noah <82201000+Nopro263@users.noreply.github.com>
2024-04-26 20:12:15 +0200

    added casino manager

---
+++ CasinoManager.cs
@@ -0,0 +1,23 @@
+﻿namespace UNSERcasino
+{
+    internal class CasinoManager
+    {
+        public double PlayerBalance { get; private set; }
+
+        private static CasinoManager? _instance;
+        public static CasinoManager Instance { get { if (_instance == null) { _instance = new CasinoManager(); } return _instance; } }
+
+        private CasinoManager() {
+            PlayerBalance = 0;
+        }
+
+        public void remove(double coins)
+        {
+            PlayerBalance -= coins;
+        }
+
+        public void add(double coins) { 
+            PlayerBalance += coins;
+        }
+    }
+}
