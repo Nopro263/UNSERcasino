e1600d90396346fc22418682d1647a46a2f72057
Noah <82201000+Nopro263@users.noreply.github.com>
2024-04-26 20:13:10 +0200

    added player balance view

---
+++ UI/PlayerBalanceView.cs
@@ -0,0 +1,15 @@
+﻿namespace UNSERcasino.UI
+{
+    internal class PlayerBalanceView : TableView, IUpdateable
+    {
+        public PlayerBalanceView() : base(new Text[][] {new Text[] {new Text("123 000"), new Text("+ 12"), new Text("- 52")} })
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
