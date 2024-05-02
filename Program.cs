e7dd0e4e6011eebcb6567e3382f7698583a0075b
Emilio <111038444+QuackingQuark@users.noreply.github.com>
2024-05-02 11:00:11 +0200

    Fixed

--- Program.cs
+++ Program.cs
@@ -5,9 +5,42 @@ namespace UNSERcasino
 {
     internal class Program
     {
+        static void WriteCentered(string text)
+        {
+            string[] splitText = text.Split('\n');
+
+            foreach (string line in splitText)
+            {
+                WriteCenteredLine(line);
+            }
+        }
+
+        static void WriteCenteredLine(string line)
+        {
+            int screenWidth = Console.WindowWidth;
+            int stringWidth = line.Length;
+            int spaces = (screenWidth - stringWidth) / 2;
+
+            for (int i = 0; i < spaces; i++)
+            {
+                Console.Write(" ");
+            }
+
+            foreach (char c in line)
+            {
+                Console.Write(c);
+                System.Threading.Thread.Sleep(100);
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
+
+            string intro = "EA Sports\nIt's in the game";
+            WriteCentered(intro);
+            System.Threading.Thread.Sleep(1000);
 
             UI.Menu.MenuManager.open(new UI.Menu.MainMenu());
 
