42b54a3b653ce8e4b8f998b39046f504c066870a
Emilio <111038444+QuackingQuark@users.noreply.github.com>
2024-05-02 10:59:31 +0200

    Mine Bug

---
+++ UI/Menu/MineMenu.cs
@@ -0,0 +1,25 @@
+﻿using UNSERcasino.Game;
+
+namespace UNSERcasino.UI.Menu
+{
+    internal class MineMenu : Menu
+    {
+        private Mines _mines;
+        private ButtonView _buttonview;
+
+        public MineMenu() : base()
+        {
+            const int col = 5;
+            const int row = 5;
+            int x = (Console.BufferWidth / 2) - (col * 10 / 2);
+            int y = (Console.BufferHeight / 2) - (row * 6 / 2);
+
+            _mines = new Mines();
+            _buttonview = new ButtonView(new Text("Reveal"), false);
+
+
+        }
+
+
+    }
+}
