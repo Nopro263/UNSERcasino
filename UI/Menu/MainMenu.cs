6c13e976598c525db1f27282f7c70a79edcb3baa
Emilio <111038444+QuackingQuark@users.noreply.github.com>
2024-05-15 20:17:48 +0200

    MainMenu Finished

--- UI/Menu/MainMenu.cs
+++ UI/Menu/MainMenu.cs
@@ -1,9 +1,26 @@
 ﻿namespace UNSERcasino.UI.Menu
 {
-    internal class MainMenu : Menu
+    internal class MainMenu : Menu, IUpdateable
     {
         public MainMenu() : base()
         {
+            scene.addView(new TextView(new Text("  _____               _____   _____   _   _    ____"), false, false), Flow.CENTER, Flow.CENTER, 0, -13); // Top Row of Characters of Casino Text
+            scene.addView(new TextView(new Text("  / ____|     /\\      / ____| |_   _| | \\ | |  / __ \\"), false, false), Flow.CENTER, Flow.CENTER, 0, -12);
+            scene.addView(new TextView(new Text(" | |         /  \\    | (___     | |   |  \\| | | |  | |"), false, false), Flow.CENTER, Flow.CENTER, 0, -11);
+            scene.addView(new TextView(new Text(" | |        / /\\ \\    \\___ \\    | |   | . ` | | |  | |"), false, false), Flow.CENTER, Flow.CENTER, 0, -10);
+            scene.addView(new TextView(new Text(" | |____   / ____ \\   ____) |  _| |_  | |\\  | | |__| |"), false, false), Flow.CENTER, Flow.CENTER, 0, -9);
+            scene.addView(new TextView(new Text("  \\_____| /_/    \\_\\ |_____/  |_____| |_| \\_|  \\____/"), false, false), Flow.CENTER, Flow.CENTER, 0, -8); // El Carino
+
+            scene.addView(new TextView(new Text("┏─────────────────┓"), false, false), Flow.CENTER, Flow.CENTER, 0, -2);
+            scene.addView(new TextView(new Text("┃                 ┃"), false, false), Flow.CENTER, Flow.CENTER, 0, -1);
+            scene.addView(new TextView(new Text("┃                 ┃"), false, false), Flow.CENTER, Flow.CENTER, 0, 0);
+            scene.addView(new TextView(new Text("┃                 ┃"), false, false), Flow.CENTER, Flow.CENTER, 0, 1);
+            scene.addView(new TextView(new Text("┃                 ┃"), false, false), Flow.CENTER, Flow.CENTER, 0, 2);
+            scene.addView(new TextView(new Text("┃                 ┃"), false, false), Flow.CENTER, Flow.CENTER, 0, 3);
+            scene.addView(new TextView(new Text("┃                 ┃"), false, false), Flow.CENTER, Flow.CENTER, 0, 4);
+            scene.addView(new TextView(new Text("┃                 ┃"), false, false), Flow.CENTER, Flow.CENTER, 0, 5);
+            scene.addView(new TextView(new Text("┗─────────────────┛"), false, false), Flow.CENTER, Flow.CENTER, 0, 6);
+
             scene.addView(new ButtonView(new Text("Crash"), false), Flow.CENTER, Flow.CENTER);
             scene.addView(new ButtonView(new Text("Dice"), false), Flow.CENTER, Flow.CENTER, 0, 1);
             scene.addView(new ButtonView(new Text("Poker"), false), Flow.CENTER, Flow.CENTER, 0, 2);
@@ -38,6 +55,11 @@
             {
                 MenuManager.close();
             }
+        }
+
+        public void Update()
+        {
+
         }
     }
 }
