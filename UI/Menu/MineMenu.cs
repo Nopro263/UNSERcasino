03b829266049abe8eb25746cd89d891e3cec49f8
Emilio <111038444+QuackingQuark@users.noreply.github.com>
2024-05-30 20:30:01 +0200

    MineMenu

--- UI/Menu/MineMenu.cs
+++ UI/Menu/MineMenu.cs
@@ -7,7 +7,7 @@ namespace UNSERcasino.UI.Menu
         private Mines _mines;
         private _2dButtonView _buttonview;
         private TextView _textview;
-
+        private TextInputView _bet;
         public MineMenu() : base()
         {
             const int col = 5;
@@ -15,6 +15,7 @@ namespace UNSERcasino.UI.Menu
             int x = (Console.BufferWidth / 2) - (col * 10 / 2);
             int y = (Console.BufferHeight / 2) - (row * 6 / 2);
 
+            _bet = new TextInputView(false, 5, "Bet");
 
 
 
@@ -24,8 +25,10 @@ namespace UNSERcasino.UI.Menu
             scene.addView(_buttonview, Flow.CENTER, Flow.CENTER);
 
             _textview = new TextView(new Text("Total Amount: "), false, false);
+            scene.addView(_bet, Flow.CENTER, Flow.CENTER, 0, 6);
             scene.addView(_textview, Flow.CENTER, Flow.CENTER, 0, 8);
             scene.addView(new ButtonView(new Text("Cashout"), false), Flow.CENTER, Flow.CENTER, 0, 10);
+
             _mines.StartGame(1);
 
 
@@ -34,21 +37,44 @@ namespace UNSERcasino.UI.Menu
         public override void onClick(IClickable button)
         {
 
-
+            ButtonView cashout = button as ButtonView;
             _2dButtonView? view = _buttonview as _2dButtonView;
             if (view != null)
             {
-                if (_mines.Play(view.X, view.Y) != 0)
+                if (_mines.Play(view.X, view.Y) != 0 && _mines.Play(view.X, view.Y) != 3)
                 {
-                    view.setChar(view.X, view.Y, 'W');
-                    _textview.Text.setContent("Multiplier: " + (_mines.Play(view.X, view.Y) )); //add Bets
+                    view.setChar(view.X, view.Y, '\u25C7');
+                    _textview.Text.setContent("Multiplier: " + (_mines.Play(view.X, view.Y)));
                 }
                 else if (_mines.Play(view.X, view.Y) == 0)
                 {
-                    view.setChar(view.X, view.Y, 'L');
+                    view.setChar(view.X, view.Y, '\u25CB');
                     _textview.Text.setContent("Multiplier: " + _mines.Play(view.X, view.Y));
-
+                    _mines.MineRevealed = true;
                 }
+                else if (_mines.Play(view.X, view.Y) == 3)
+                {
+                    _textview.Text.setContent("Game Over");
+                }
+            }
+            if (cashout != null)
+            {
+
+                if (int.TryParse(_bet.FullContent) != null)
+                {
+                    if (!_mines.MineRevealed)
+                    {
+
+                        _textview.Text.setContent("You won: " + _mines.CalcMultiplier() * int.Parse(_bet.FullContent));
+                        CasinoManager.Instance.add(_mines.CalcMultiplier() * int.Parse(_bet.FullContent));
+                    }
+                    else
+                    {
+                        _textview.Text.setContent("You lost: " + (-int.Parse(_bet.FullContent)));
+                        CasinoManager.Instance.add(-int.Parse(_bet.FullContent));
+                    }
+                }
+                else { _textview.Text.setContent("Please enter a valid bet"); }
             }
         }
     }
