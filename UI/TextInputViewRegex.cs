4cc24fc492fb3389ed089055ff5d94880427429d
Emilio Schwaiger <111038444+QuackingQuark@users.noreply.github.com>
2024-06-04 20:21:09 +0200

    Added TextInputViewRegex + A UML-Class Diagram as a Test

---
+++ UI/TextInputViewRegex.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UNSERcasino.UI
+{
+    internal class TextInputViewRegex : TextInputView
+    {
+        Regex rx = new Regex(@"^[0-9]+$");
+        public TextInputViewRegex(bool vertical, int maxLen, string placeholder) : base(vertical, maxLen, placeholder)
+        {
+        }
+
+        public override void onKey(ConsoleKeyInfo key)
+        {
+            if (key.Key == ConsoleKey.Backspace)
+            {
+                if (_fullContent.Length > 0 && !_atStart)
+                {
+                    _fullContent = _fullContent.Substring(0, _fullContent.Length - 1); // remove the last Element
+                }
+
+                if (_fullContent.Length == 0)
+                {
+                    _fullContent = _placeholder;
+                    _atStart = true;
+                }
+
+
+            }
+            else
+            {
+                if (_atStart)
+                {
+                    _atStart = false;
+                    _fullContent = "";
+                }
+                if (rx.IsMatch(key.KeyChar.ToString())) // check if the key is a number
+                {
+                    _fullContent += key.KeyChar.ToString(); // add it
+                }
+            }
+        }
+    }
+}
